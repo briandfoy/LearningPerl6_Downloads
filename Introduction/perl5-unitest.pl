7613e5bb57963e504a969917a2424798c426b2ed
brian d foy <brian.d.foy@gmail.com>
2018-01-18 12:53:34 -0500

    Add a Perl 5 version of the unitest

---
+++ Introduction/perl5-unitest.pl
@@ -0,0 +1,22 @@
+#!perl
+
+print <<'END';
+Basic accents and things--------------
+--------------------------------------
+Leftward Double Arrow (⇐, 0x21D0)
+Rightward Double Arrow (⇒, 0x21D2)
+When you want a job you prepare your résumé
+Hello Mr Sørensen
+
+Things that matter to Perl 6----------
+--------------------------------------
+Corner brackets: 「 」
+Fancy double list quotes:  « »
+Terms: π τ ∞
+Exponents: ⁰ ¹ ⁹
+
+Emoji things -------------------------
+--------------------------------------
+Thumbs up: 👎
+Butterfly: 🦋
+END
